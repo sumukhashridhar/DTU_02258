f03ccb733ab8e8bc1e38231a7af0f6a118c6c1fb
sumukhahegde <5087581-sumukhahegde@users.noreply.gitlab.com>
2022-11-15 16:41:22 +0100

    MPI working, ans wrong

--- mpiCalc.c
+++ mpiCalc.c
@@ -6,13 +6,12 @@
 #define INTERVALS 10000000
 
 double a[INTERVALS], b[INTERVALS];
-  // double from[INTERVALS];
 
 int main(int argc, char **argv)
 {
   double time2;
   time_t time1 = clock();
-  double from[INTERVALS];
+  double *from = a;
 
   int rank, size;
 
@@ -26,10 +25,6 @@ int main(int argc, char **argv)
   int    time_steps = 100;
   long int points_per_domain = INTERVALS / size;
 
-  // double from_local[points_per_domain + 2];
-  // double to_local[points_per_domain + 2];
-  // double temp_local[points_per_domain + 2];
-
   int start = 0;
   long int end = points_per_domain + 1;
 
@@ -40,18 +35,19 @@ int main(int argc, char **argv)
 
   /* Set up initial and boundary conditions. */
 
-          for(long i = 0; i <= end; i++)
+          for(long int i = 1; i < end; i++)
             from_local[i] = 0.0;
 
           // if (rank == 0) {
             from_local[0] = 1.0;
-            to_local[0] = from_local[0];
+            // to_local[0] = from_local[0];
           // }
 
           // if (rank == size - 1) {
             from_local[end] = 0.0;
-            to_local[end] = from_local[end];
+            // to_local[end] = from_local[end];
           // }
+    // MPI_Barrier(MPI_COMM_WORLD);
 
   while(time_steps-- > 0)
   {
@@ -60,31 +56,32 @@ int main(int argc, char **argv)
       MPI_Recv(&from_local[start], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
 
     if (rank < size - 1)
-      MPI_Send(&from_local[end-1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
+      MPI_Send(&from_local[end], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    // MPI_Barrier(MPI_COMM_WORLD);
 
     if (rank < size - 1)
       MPI_Recv(&from_local[end], 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &status);
 
     if (rank > 0)
-      MPI_Send(&from_local[start+1], 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
+      MPI_Send(&from_local[start], 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
 
     // MPI_Barrier(MPI_COMM_WORLD);
 
       // printf("rank is: %d, timeStep is: %d\n", rank, time_steps);
 
-   for(long i = 1; i < (end); i++)
-    to_local[i] = from_local[i] + 0.1*(from_local[i - 1] - 2*from_local[i] + from_local[i + 1]);
+   for(long int i = 1; i < end - 1 ; i++)
+    to_local[i] = from_local[i] + 0.1*(from_local[i - 1] - 2.0*from_local[i] + from_local[i + 1]);
     
-    // printf("the to_local is: %lf, rank is: %d\n", from_local[0], rank);
+  //   // printf("the to_local is: %lf, rank is: %d\n", from_local[0], rank);
 
-  //  for(long i = 1; i < (end); i++)
-  //   from_local[i] = to_local[i];
+  // //  for(long i = 1; i < (end); i++)
+  // //   from_local[i] = to_local[i];
 
     double *temp_ptr = to_local;
     to_local = from_local;
     from_local = temp_ptr;
+    // from_local = to_local;
 
   }
 
@@ -92,7 +89,7 @@ int main(int argc, char **argv)
              from, points_per_domain, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
   //                if (rank == 0) {
-  // for(long i = 2; i < 30; i += 2)
+  // for(long int i = 2; i < 12; i += 2)
   //  printf("Interval %ld: %f\n", i, from[i]);
   //            }
 
@@ -103,8 +100,8 @@ int main(int argc, char **argv)
   printf("Elapsed time (s) = %f\n", time2);
 
 
-  // for(long i = 2; i < 30; i += 2)
-  //  printf("Interval %ld: %f\n", i, to_local[i]);
+  for(long i = 2; i < 30; i += 2)
+   printf("Interval %ld: %f\n", i, to_local[i]);
   
   return 0;
 }                
