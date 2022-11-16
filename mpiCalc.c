ae84a97592032442418b3ff4cb28b51074ed400a
djangohegde <s221654@dtu.dk>
2022-11-16 12:42:36 +0100

    final MPI versoin

--- mpiCalc.c
+++ mpiCalc.c
@@ -10,7 +10,8 @@ double a[INTERVALS], b[INTERVALS];
 int main(int argc, char **argv)
 {
   double time2;
-  time_t time1 = clock();
+  // time_t time1 = clock();
+  double startT = MPI_Wtime();
   double *from = a;
 
   int rank, size;
@@ -38,40 +39,39 @@ int main(int argc, char **argv)
           for(long int i = 1; i < end; i++)
             from_local[i] = 0.0;
 
-          // if (rank == 0) {
+          if (rank == 0) {
             from_local[0] = 1.0;
-            // to_local[0] = from_local[0];
-          // }
+            to_local[0] = from_local[0];
+          }
 
-          // if (rank == size - 1) {
+          if (rank == size - 1) {
             from_local[end] = 0.0;
-            // to_local[end] = from_local[end];
-          // }
+            to_local[end] = from_local[end];
+          }
     // MPI_Barrier(MPI_COMM_WORLD);
 
   while(time_steps-- > 0)
   {
 
-    if (rank > 0) 
+    if (rank > 0) {
       MPI_Recv(&from_local[start], 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &status);
+      MPI_Send(&from_local[start+1], 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
+    }
 
-    if (rank < size - 1)
-      MPI_Send(&from_local[end], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
-
-    // MPI_Barrier(MPI_COMM_WORLD);
-
-    if (rank < size - 1)
-      MPI_Recv(&from_local[end], 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &status);
-
-    if (rank > 0)
-      MPI_Send(&from_local[start], 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
+    if (rank < size - 1){
+      MPI_Send(&from_local[end-1], 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
+       MPI_Recv(&from_local[end], 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &status);
+    }
 
     // MPI_Barrier(MPI_COMM_WORLD);
+
+
+    MPI_Barrier(MPI_COMM_WORLD);
 
       // printf("rank is: %d, timeStep is: %d\n", rank, time_steps);
 
-   for(long int i = 1; i < end - 1 ; i++)
-    to_local[i] = from_local[i] + 0.1*(from_local[i - 1] - 2.0*from_local[i] + from_local[i + 1]);
+   for(long int i = 1; i < end; i++)
+   	to_local[i] = from_local[i] + 0.1*(from_local[i - 1] - 2.0*from_local[i] + from_local[i + 1]);
     
   //   // printf("the to_local is: %lf, rank is: %d\n", from_local[0], rank);
 
@@ -92,16 +92,18 @@ int main(int argc, char **argv)
   // for(long int i = 2; i < 12; i += 2)
   //  printf("Interval %ld: %f\n", i, from[i]);
   //            }
+time2 = MPI_Wtime() - startT;
 
+if(rank == 0)
+        printf("Elapsed time (s) = %f\n", time2);
   MPI_Finalize();
 
-  time2 = (clock() - time1) / (double) CLOCKS_PER_SEC;
+  // time2 = (clock() - time1) / (double) CLOCKS_PER_SEC;
 
-  printf("Elapsed time (s) = %f\n", time2);
+  // printf("Elapsed time (s) = %f\n", time2);
 
-
-  for(long i = 2; i < 30; i += 2)
-   printf("Interval %ld: %f\n", i, to_local[i]);
+  // for(long i = 2; i < 30; i += 2)
+  //  printf("Interval %ld: %f\n", i, to_local[i]);
   
   return 0;
 }                
